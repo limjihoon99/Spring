ea419b80f69ed12a3adaace4dda47ac148a639c9
Jihoon Lim <121468393+limjihoon99@users.noreply.github.com>
2024-07-15 10:10:43 +0900

    Delete 03_SpringMVC directory

--- 03_SpringMVC/src/main/java/com/beyond/mvc/member/model/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.mvc.member.model.repository;
-
-import com.beyond.mvc.member.model.vo.Member;
-
-public interface MemberRepository {
-	
-	Member findMemberById(String userId);
-
-}
--- 03_SpringMVC/src/main/java/com/beyond/mvc/member/model/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.mvc.member.model.service;
-
-import com.beyond.mvc.member.model.vo.Member;
-
-public interface MemberService {
-	
-	Member login(String userId, String userPwd);
-
-}
